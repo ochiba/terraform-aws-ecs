2ea5158ffeabf71aab40b4e3a00104b55cb34988
Hiroki KUNITAKE <feuerwerk.des.winters@gmail.com>
2021-07-23 18:26:38 +0900

    add ECS Execution role and Task role

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,115 @@
+resource "aws_iam_service_linked_role" "ecs" {
+  aws_service_name = "ecs.amazonaws.com"
+}
+
+# IAM Role for ECS Service
+data "aws_iam_policy_document" "ecs_service_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      # Rules which allow ECS to attach network interfaces to instances
+      # on your behalf in order for awsvpc networking mode to work right
+      "ec2:AttachNetworkInterface",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateNetworkInterfacePermission",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DeleteNetworkInterfacePermission",
+      "ec2:Describe*",
+      "ec2:DetachNetworkInterface",
+      # Rules which allow ECS to update load balancers on your behalf
+      # with the information sabout how to send traffic to your containers
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:RegisterTargets",
+      # Rules which allow ECS to run tasks that have IAM roles assigned to them.
+      "iam:PassRole",
+      # Rules that let ECS interact with container images.
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      # Rules that let ECS create and push logs to CloudWatch.
+      "logs:DescribeLogStreams",
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecs_service_role" {
+  name   = "myECSServiceRolePolicy"
+  policy = data.aws_iam_policy_document.ecs_service_role.json
+}
+
+data "aws_iam_policy_document" "ecs_service_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = [
+        "ecs.amazonaws.com",
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_service" {
+  name = "myECSServiceRole"
+  assume_role_policy  = data.aws_iam_policy_document.ecs_service_assume_role.json
+  managed_policy_arns = [
+    aws_iam_policy.ecs_service_role.arn
+  ]
+}
+
+# IAM Role for ECS Task
+data "aws_iam_policy_document" "ecs_task_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      # Allow the ECS Tasks to download images from ECR
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      # Allow the ECS tasks to upload logs to CloudWatch
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }  
+}
+
+resource "aws_iam_policy" "ecs_task_role" {
+  name   = "myECSTaskRolePolicy"
+  policy = data.aws_iam_policy_document.ecs_task_role.json
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task" {
+  name = "myECSTaskRole"
+  assume_role_policy  = data.aws_iam_policy_document.ecs_task_assume_role.json
+  managed_policy_arns = [
+    aws_iam_policy.ecs_task_role.arn
+  ]
+}
+
 resource "aws_security_group" "ecs" {
   name        = "${var.name}-sg-ecs"
   description = "SG for ${var.name} ecs"
@@ -29,6 +141,9 @@ resource "aws_ecs_task_definition" "main" {
   family = var.name
   requires_compatibilities = ["FARGATE"]
   network_mode = "awsvpc"
+
+  execution_role_arn = aws_iam_role.ecs_service
+  task_role_arn      = aws_iam_role.ecs_task
 
   cpu = 256
   memory = 512
